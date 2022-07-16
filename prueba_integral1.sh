1f2e7cd774fad1f20439890d96ad59344b23f0fe
Emmanuel <emmanuel.lazarte.012@gmail.com>
2022-07-16 03:31:43 -0300

    Añadido: prueba integral

---
+++ prueba_integral1.sh
@@ -0,0 +1,1 @@
+make exec-consola ARCHIVO=/home/utnso/tp-2022-1c-Sisop-Oh-Yeah/project/consola/config/INTEGRAL_1.txt TAMANIO=2048
---
+++ prueba_integral2.sh
@@ -0,0 +1,1 @@
+make exec-consola ARCHIVO=/home/utnso/tp-2022-1c-Sisop-Oh-Yeah/project/consola/config/INTEGRAL_2.txt TAMANIO=2048
